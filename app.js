91c0d43f54ba06a2e919add6db50344af88da6bf
anjalitiwari17 <anjalitiwari152004@gmail.com>
2024-04-06 11:44:19 +0530

    commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// console.log("hello".includes("o"))
+// function greet(){
+//     let fname='john';
+//     console.log('hello'+fname);
+// }
+// greet();
+// console.log(fname);
+// greet();
+
+
+let fname="john";
+function greet(){
+    let fname="sushmita";
+    console.log('hello' +fname);
+
+}
+console.log(fname)
+greet()
+console.log(fname)
